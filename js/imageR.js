4b3bc3e1d0f24c5428d9659e7f289e109153c9c1
xiaohong <805642585@qq.com>
2015-09-30 17:45:32 +0800

    v5.0

--- js/imageR.js
+++ js/imageR.js
@@ -5,6 +5,8 @@
 		alert('log');
 	}
 });*/
+//自调用包裹住函数，并传入适当的参数
+;(function(window,document,$,undefined){
 function produceTable(pathArray,width,height){
 	$table = $("<table class='imagesT'></table>");
 	var length = pathArray.length;
@@ -22,25 +24,29 @@ function produceTable(pathArray,width,height){
 	$oTdF1 = $("<td id='F1'></td>");
 	$oTdF2 = $("<td id='F2'></td>");
 	$oTdF1=$table.appendTo($oTdF1);
+	//console.log("table: "+$table.html());
 	$oTr = $oTdF1.appendTo($oTr);
 	console.log("td1: "+$oTdF1.html());
 	//$oTdF2.html($oTdF1.html());
 	$oTdF2=$table.appendTo($oTdF2);
 	console.log("td2: "+$oTdF2.html());
 	$oTdF2.appendTo($oTr);
-	//console.log($oTr.html());
-	$oTr.appendTo($oTable);
+	console.log("$otr: "+$oTr.html());
+	$oTable=$oTr.appendTo($oTable);
+	console.log("$otable: "+$oTable.html());
 	return $oTable;
 };
-function runLeft(width,stime){
-	//获取容器的宽度		 
-	setInterval("run()",100);
-};
-function run(){
+function Run(){
 		//var tab = window.document.getElementById('FF');
 		var tab = document.getElementById("FF")
 		tab.scrollLeft++;
 }
+function runLeft(width,stime){
+	//获取容器的宽度
+	var run = new Run();		 
+	setInterval("run()",100);
+};
+
 //第二种扩展方法，可以调用jquery的选择器功能
 $.fn.imagesR=function(pathArray,width,height){
 	var defaultPath = {};
@@ -55,3 +61,4 @@ $.fn.imagesR=function(pathArray,width,height){
 	//alert("容器宽度为："+cwidth);
 	runLeft(parseInt(cwidth),100);
 }
+})(window,document,jQuery,undefined);
