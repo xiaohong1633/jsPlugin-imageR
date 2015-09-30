012bba034521f45f2b8308ff1fb287108e979f76
xiaohong <805642585@qq.com>
2015-09-30 17:21:41 +0800

    v4.0

--- js/image.js
+++ js/image.js
@@ -1,1 +1,8 @@
-/*image.js*/
+$(document).ready(function(){
+//$.log();
+var pathArray = new Array("/jsPlugin-imageR/images/1.jpg",
+	"/jsPlugin-imageR/images/2.jpg","/jsPlugin-imageR/images/3.jpg",
+	"/jsPlugin-imageR/images/4.jpg","/jsPlugin-imageR/images/5.jpg");
+//alert(pathArray.length);
+$(".imagesR").imagesR(pathArray,200,150);
+});
---
+++ js/imageR.js
@@ -0,0 +1,57 @@
+/*image.js*/
+//第一种扩展方式，直接给jquery扩展中传入参数方法对象
+/*$.extend({
+	log:function(){
+		alert('log');
+	}
+});*/
+function produceTable(pathArray,width,height){
+	$table = $("<table class='imagesT'></table>");
+	var length = pathArray.length;
+	//alert(length)
+	var tr = "<tr >";
+	for(var i=0;i<length;i++){
+		tr += "<td><img width='"+width+"' height='"+height+"' src='"+pathArray[i]+"'/></td>"
+	}
+	tr=tr+"</tr>";
+	//console.log(tr);
+	$table = $(tr).appendTo($table);
+	//初始化外层table
+	$oTable = $("<table></table>");
+	$oTr = $("<tr id='FF'></tr>");
+	$oTdF1 = $("<td id='F1'></td>");
+	$oTdF2 = $("<td id='F2'></td>");
+	$oTdF1=$table.appendTo($oTdF1);
+	$oTr = $oTdF1.appendTo($oTr);
+	console.log("td1: "+$oTdF1.html());
+	//$oTdF2.html($oTdF1.html());
+	$oTdF2=$table.appendTo($oTdF2);
+	console.log("td2: "+$oTdF2.html());
+	$oTdF2.appendTo($oTr);
+	//console.log($oTr.html());
+	$oTr.appendTo($oTable);
+	return $oTable;
+};
+function runLeft(width,stime){
+	//获取容器的宽度		 
+	setInterval("run()",100);
+};
+function run(){
+		//var tab = window.document.getElementById('FF');
+		var tab = document.getElementById("FF")
+		tab.scrollLeft++;
+}
+//第二种扩展方法，可以调用jquery的选择器功能
+$.fn.imagesR=function(pathArray,width,height){
+	var defaultPath = {};
+	//参数的第一个空对象是为了保护好默认参数不被污染
+	//需注意合并的是对象而不是数组，若是数组，则会合并失败
+	//var pathArrays = $.extend({},defaultPath,pathArray);
+	//alert(pathArrays.length);
+	//调用一个函数产生table
+	produceTable(pathArray,width,height).appendTo(this);
+	//对产生的table中的td向左侧移动，并触发时间
+	var cwidth = this.css("width");
+	//alert("容器宽度为："+cwidth);
+	runLeft(parseInt(cwidth),100);
+}
