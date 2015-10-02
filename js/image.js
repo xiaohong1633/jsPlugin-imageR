580d77e9faaa824bc79a01d88459729e0bcfec71
xiaohong <805642585@qq.com>
2015-10-02 13:45:51 +0800

    v5.0

--- js/image.js
+++ js/image.js
@@ -1,8 +1,11 @@
-$(document).ready(function(){
+ 
+
+$(function(){
 //$.log();
 var pathArray = new Array("/jsPlugin-imageR/images/1.jpg",
 	"/jsPlugin-imageR/images/2.jpg","/jsPlugin-imageR/images/3.jpg",
 	"/jsPlugin-imageR/images/4.jpg","/jsPlugin-imageR/images/5.jpg");
 //alert(pathArray.length);
-$(".imagesR").imagesR(pathArray,200,150);
+//插件调用方式:传入要插入的元素ID ，传入图片路径数组，并传入每张照片的大小width，height
+jQuery(".imagesR").imagesR(pathArray,200,150);
 });
--- js/imageR.js
+++ js/imageR.js
@@ -8,57 +8,81 @@
 //自调用包裹住函数，并传入适当的参数
 ;(function(window,document,$,undefined){
 function produceTable(pathArray,width,height){
-	$table = $("<table class='imagesT'></table>");
+	//alert($('body').text());
+	//$证明 jquery对象已经成功传入！
+	this.oTable = $("<table></table>");
+	this.oTr = $("<tr id='FF'></tr>");
+	this.oTdF1 = $("<td id='F1'></td>");
+	this.oTdF2 = $("<td id='F2'></td>");
+	//var table = "";
 	var length = pathArray.length;
 	//alert(length)
-	var tr = "<tr >";
+	var tr = "<table class='imagesT'><tr >";
 	for(var i=0;i<length;i++){
-		tr += "<td><img width='"+width+"' height='"+height+"' src='"+pathArray[i]+"'/></td>"
+		tr += "<td><img width='"+width+"' height='"+height+"' src='"+pathArray[i]+"'/></td>";
 	}
-	tr=tr+"</tr>";
+	tr=tr+"</tr></table>";
 	//console.log(tr);
-	$table = $(tr).appendTo($table);
+	this.table = $(tr);
+	//console.log("tr: "+table.html());
+	//table = tr.appendTo(table);
+	//return $table;
+	//console.log("table: "+this.table.html());
 	//初始化外层table
-	$oTable = $("<table></table>");
-	$oTr = $("<tr id='FF'></tr>");
-	$oTdF1 = $("<td id='F1'></td>");
-	$oTdF2 = $("<td id='F2'></td>");
-	$oTdF1=$table.appendTo($oTdF1);
+	
+	(this.table).appendTo(this.oTdF1);
 	//console.log("table: "+$table.html());
-	$oTr = $oTdF1.appendTo($oTr);
-	console.log("td1: "+$oTdF1.html());
+	this.oTdF1.appendTo(this.oTr);
+	//console.log("td1: "+this.oTdF1.html());
+	//console.log("otr: "+this.oTr.html());
 	//$oTdF2.html($oTdF1.html());
-	$oTdF2=$table.appendTo($oTdF2);
-	console.log("td2: "+$oTdF2.html());
-	$oTdF2.appendTo($oTr);
-	console.log("$otr: "+$oTr.html());
-	$oTable=$oTr.appendTo($oTable);
-	console.log("$otable: "+$oTable.html());
-	return $oTable;
+	//(this.table).appendTo(this.oTdF2);
+	//console.log("td2: "+this.oTdF2.html());
+	//console.log("table: "+this.table.html());
+	this.oTdF2.appendTo(this.oTr);
+	//console.log("otr: "+this.oTr.html());
+	this.oTr.appendTo(this.oTable);
+	//console.log("otable: "+this.oTable.html());
+	return this.oTable;
 };
-function Run(){
+function run(){
 		//var tab = window.document.getElementById('FF');
-		var tab = document.getElementById("FF")
+		var tab = document.getElementById("parent");
+		var tab1 = document.getElementById("F1");
+		var tab2 = document.getElementById("F2");
+		if(tab1.offsetWidth-tab.scrollLeft<0){
+			tab.scrollLeft = 0;	
+		}else{
 		tab.scrollLeft++;
+		}
 }
-function runLeft(width,stime){
-	//获取容器的宽度
-	var run = new Run();		 
-	setInterval("run()",100);
+function runLeft(stime){
+	//获取容器的宽度,直接写方法名称即可不用加引号		 
+	return setInterval(run,stime);
 };
-
+function stopRun(){
+	clearInterval(intervalFlag);	
+}
+$("body").off("mouseover","#parent").on("mouseover","#parent",function(){
+	//alert("mouserover");
+    stopRun();
+});
+$("body").off("mouseout","#parent").on("mouseout","#parent",function(){
+	//alert("mouserover");
+    intervalFlag = runLeft(10);
+});
+var intervalFlag;
 //第二种扩展方法，可以调用jquery的选择器功能
 $.fn.imagesR=function(pathArray,width,height){
-	var defaultPath = {};
+	//var defaultPath = {};
 	//参数的第一个空对象是为了保护好默认参数不被污染
 	//需注意合并的是对象而不是数组，若是数组，则会合并失败
-	//var pathArrays = $.extend({},defaultPath,pathArray);
-	//alert(pathArrays.length);
 	//调用一个函数产生table
 	produceTable(pathArray,width,height).appendTo(this);
+	$("#F2").append($("#F1").html());
 	//对产生的table中的td向左侧移动，并触发时间
 	var cwidth = this.css("width");
-	//alert("容器宽度为："+cwidth);
-	runLeft(parseInt(cwidth),100);
+	intervalFlag = runLeft(10);
 }
-})(window,document,jQuery,undefined);
+})(window,document,jQuery);
+
